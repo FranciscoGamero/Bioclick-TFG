8c5eaecf0451ed1405ba34a1b0a77278d5def33b
Francisco_Gamero <126238831+FranciscoGamero@users.noreply.github.com>
2025-06-06 18:26:13 +0200

    Se ha cambiado el dto de usuario para poder editarlo en el frontend

--- Backend/Api/src/main/java/com/salesianostriana/bioclick/dto/manager/ManagerDto.java
+++ Backend/Api/src/main/java/com/salesianostriana/bioclick/dto/manager/ManagerDto.java
@@ -3,18 +3,23 @@ package com.salesianostriana.bioclick.dto.manager;
 import com.salesianostriana.bioclick.model.Manager;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public record ManagerDto(
+        UUID id,
         String username,
         String correo,
+        String password,
         String fotoPerfilUrl,
         String ultimaAccion,
         LocalDateTime fechaUltimaAccion
 ) {
     public static ManagerDto of(Manager manager, String urlImagen){
         return new ManagerDto(
+                manager.getId(),
                 manager.getUsername(),
                 manager.getCorreo(),
+                manager.getPassword(),
                 urlImagen,
                 manager.getUltimaAccionProducto(),
                 manager.getFechaUltimaAccionProducto()
@@ -22,8 +27,10 @@ public record ManagerDto(
     }
     public static ManagerDto of(Manager manager){
         return new ManagerDto(
+                manager.getId(),
                 manager.getUsername(),
                 manager.getCorreo(),
+                manager.getPassword(),
                 manager.getFotoPerfil(),
                 manager.getUltimaAccionProducto(),
                 manager.getFechaUltimaAccionProducto()
